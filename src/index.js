a98d2ea583986ae3c29cc3cce97979a57234d983
Orlovsky_Oleg <oorlovsky86@gmail.com>
2019-10-02 19:58:13 +0300

    feat: first stage compleat
    3 test is compleat.
    need to use large numbers

---
+++ src/index.js
@@ -0,0 +1,16 @@
+module.exports = function multiply(first, second) {
+  let numFirst = Number(first);
+  let numSecond = Number(second);
+  return String(numFirst * numSecond);
+}
+
+/*function mult(str1, str2) {
+    let numFirst = Number(str1);
+    let numSecond = Number(str2);
+    let res = numFirst * numSecond;
+    toString(res);
+    alert(res)
+    return numFirst * numSecond;
+}
+
+mult('5', '10');*/
