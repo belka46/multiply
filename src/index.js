efd13fe3e28c718074bbde5d87617facce134e70
Orlovsky_Oleg <oorlovsky86@gmail.com>
2019-10-02 22:41:58 +0300

    feat: all test conpleat
    solution found. work with strings and column multiplication

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 module.exports = function multiply(first, second) {
-  let numFirst = Number(first);
-  let numSecond = Number(second);
-  return String(numFirst * numSecond);
+    let arrStr1 = first.split('').reverse();
+    let arrStr2 = second.split('').reverse();
+    let mult = [];
+    for (let i = 0; i < arrStr1.length; i++) {
+        for (let j = 0; j < arrStr2.length; j++) {
+            let res = arrStr1[i] * arrStr2[j];
+            if(mult[i + j]){
+                mult[i + j] += res;
+            }else mult[i + j] = res;
+        }
+    }
+    for (let i = 0; i < mult.length; i++) {
+        let num = mult[i] % 10;
+        let move = Math.floor(mult[i] / 10);
+        mult[i] = num;
+        if (mult[i + 1]){
+            mult[i + 1] += move;
+        }else if (move){
+            mult[i + 1] = move;
+        }
+    }
+    return mult.reverse().join('');
 }
-
-/*function mult(str1, str2) {
-    let numFirst = Number(str1);
-    let numSecond = Number(str2);
-    let res = numFirst * numSecond;
-    toString(res);
-    alert(res)
-    return numFirst * numSecond;
-}
-
-mult('5', '10');*/
